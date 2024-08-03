615cf0b08f419d25e15560254624474d4731cf78
Sid <singhsiddharth101251@gmail.com>
2024-08-03 07:11:24 +0530

    added express cors body-parser package

---
+++ index.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+// Access your API key as an environment variable
+const genAI = new GoogleGenerativeAI(process.env.REACT_APP_GOOGLE_GEMINI_API);
+console.log(genAI);
+
+async function run() {
+  // The Gemini 1.5 models are versatile and work with both text-only and multimodal prompts
+  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+  const prompt = "Write a story about a magic backpack.";
+
+  const result = await model.generateContent(prompt);
+  const response = await result.response;
+  const text = response.text();
+  console.log(text);
+}
+
+run();
